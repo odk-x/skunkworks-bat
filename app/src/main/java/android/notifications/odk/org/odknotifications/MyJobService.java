2a9c91ed93358b4d94f8575b3cb3f6e92728f23b
laddha-adi <f2016038@pilani.bits-pilani.ac.in>
2018-05-22 21:15:51 +0530

    Initial commit

---
+++ app/src/main/java/android/notifications/odk/org/odknotifications/MyJobService.java
@@ -0,0 +1,26 @@
+package android.notifications.odk.org.odknotifications;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+public class MyJobService extends JobService {
+
+    private static final String TAG = "MyJobService";
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        Log.d(TAG, "Performing long running task in scheduled job");
+        // TODO(developer): add long running task here.
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
