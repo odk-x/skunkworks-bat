52e27645a97363f1029e9c743b79feee660d5daf
shubhamr837 <sremail2010@gmail.com>
2020-03-25 21:05:10 +0530

    Added Exeption Handling

--- app/src/main/java/org/odk/odknotifications/Activities/MainActivity.java
+++ app/src/main/java/org/odk/odknotifications/Activities/MainActivity.java
@@ -334,27 +334,39 @@ public class MainActivity extends AppCompatActivity
         dialog.setMessage("Please Wait! Loading groups from firebase...");
         dialog.show();
         dialog.setCancelable(false);
-        DatabaseReference mRef  = FirebaseDatabase.getInstance().getReference().child("clients").child(loggedInUsername).child("groups");
-
-        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot group: dataSnapshot.getChildren()){
-                    String name =(String) group.child("name").getValue();
-                    String id = (String) group.child("id").getValue();
-                    Group grp = new  Group(id,name,0);
-                    groupArrayList.add(grp);
-                    new SubscribeNotificationGroup(MainActivity.this, id, loggedInUsername).execute();
-                    dbHandler.addNewGroup(grp);
+        try {
+            DatabaseReference mRef = FirebaseDatabase.getInstance().getReference().child("clients").child(loggedInUsername).child("groups");
+            mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot group : dataSnapshot.getChildren()) {
+                        String name = (String) group.child("name").getValue();
+                        String id = (String) group.child("id").getValue();
+                        Group grp = new Group(id, name, 0);
+                        groupArrayList.add(grp);
+                        new SubscribeNotificationGroup(MainActivity.this, id, loggedInUsername).execute();
+                        dbHandler.addNewGroup(grp);
+                    }
+                    dialog.dismiss();
                 }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            Toast.makeText(this,"Unkown Error Occured",Toast.LENGTH_SHORT);
+        }
+        finally {
+            if(dialog.isShowing());
+            {
+
                 dialog.dismiss();
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
