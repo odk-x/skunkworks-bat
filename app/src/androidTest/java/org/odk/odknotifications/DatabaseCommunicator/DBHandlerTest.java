991df2aec873e300f09045d2dd18f2e7be3d18ce
codeknight3 <vyankateshs33@gmail.com>
2020-08-20 19:02:09 +0530

    Added Sync Data Test

--- app/src/androidTest/java/org/odk/odknotifications/DatabaseCommunicator/DBHandlerTest.java
+++ app/src/androidTest/java/org/odk/odknotifications/DatabaseCommunicator/DBHandlerTest.java
@@ -1,7 +1,7 @@
 package org.odk.odknotifications.DatabaseCommunicator;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.After;
@@ -11,14 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.odk.odknotifications.Model.Group;
 import org.odk.odknotifications.Model.Notification;
+import org.odk.odknotifications.SyncDataWithServices;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static junit.framework.TestCase.assertNotNull;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -74,5 +75,21 @@ public class DBHandlerTest{
         Assert.assertTrue(EqualsBuilder.reflectionEquals(group2,groups.get(1)));
     }
 
+    @Test
+    public void syncDataTest() {
+
+        Notification notification1 = new Notification("test1", "test_notification1", "This is a test notification1", Calendar.getInstance().getTimeInMillis(), "test_group", Notification.SIMPLE, null);
+        Notification notification2 = new Notification("test2", "test_notification2", "This is a test notification2", Calendar.getInstance().getTimeInMillis(), "test_group", Notification.SIMPLE, null);
+
+        ArrayList<Notification> notificationArrayList = new ArrayList<>();
+        notificationArrayList.add(notification1);
+        notificationArrayList.add(notification2);
+
+        new SyncDataWithServices(notificationArrayList, dbHandler).syncData();
+        List<Notification> notifications = dbHandler.getAllNotificationsWithResponses();
+
+        Assert.assertTrue(EqualsBuilder.reflectionEquals(notification1, notifications.get(0)));
+        Assert.assertTrue(EqualsBuilder.reflectionEquals(notification2, notifications.get(1)));
+    }
 
 }
